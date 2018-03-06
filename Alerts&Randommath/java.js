c7dea82544ab4df29a1d70acc5a518747a737d31
jbreznik <joshbreznik@outlook.com>
2018-03-05 21:26:12 -0700

    Random.Math

---
+++ Alerts&Randommath/java.js
@@ -0,0 +1,26 @@
+<script>
+	var x;
+	x = 2;
+	document.getElementById("demo").innerHTML = x;
+	</script><script>
+	var x;
+	x = 2;
+	document.getElementById("demo").innerHTML = x;
+	</script><script>
+	var x;
+	x = 2;
+	document.getElementById("demo").innerHTML = x;
+	</script><script>
+	var x;
+	x = 2;
+	document.getElementById("demo").innerHTML = x;
+	</script><script>
+	var x;
+	x = 2;
+	document.getElementById("demo").innerHTML = x;
+	</script>// JavaScript Document
+
+var x = 5;
+var y = 2;
+var z = x + y;
+document.getElementById("demo").innerhtml = z;
